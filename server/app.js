402e09a2d43b78139d348117207876a86f6b1ad8
leoyongyuan <1359195795@qq.com>
2024-04-26 16:37:06 +0800

    chore: update app.js

--- server/app.js
+++ server/app.js
@@ -2,4 +2,7 @@ const express = require('express')
 const app = express()
 const port = 3000 // 自定义端口号（不要与已存在端口冲突）
 app.use(express.static('../dist')) // dist 是项目的打包资源路径，一般为根目录下dist
+app.get("*", (res,err) => {
+    console.log("test")
+})
 app.listen(port, () => console.log(`服务器 ${port} 开启成功!`))
