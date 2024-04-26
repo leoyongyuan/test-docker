3b7055adfac5f00a64ff3074e3bd54b95fcc103d
leoyongyuan <1359195795@qq.com>
2024-04-26 15:36:47 +0800

    chore: update build path

--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    rollupOptions: {
+      output: [
+        {
+          dir: './test-docker',
+        }
+      ]
+    }
+  },
   plugins: [react()],
 })
