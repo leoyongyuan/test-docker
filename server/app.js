915b5f740aede41ff75b602bf610d414dfb27298
leoyongyuan <1359195795@qq.com>
2024-04-29 19:00:08 +0800

    chore: update server get static source

--- server/app.js
+++ server/app.js
@@ -2,11 +2,12 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
+const root = "./";
 app.use(express.static("./dist"));
 app.listen(port, () => console.log(`服务器 ${port} 开启成功!`));
 app.get("*", (req, res) => {
-	res.sendFile(path.join("./", "dist", "index.html"));
+	res.sendFile("index.html", { root: "./dist" });
 });
 const TAG_VERSION_ENV = process.env.TAG_VERSION_ENV;
 console.log(`My variable is: ${TAG_VERSION_ENV}`);
-console.log(path.join(__dirname, "dist"), __dirname);
+console.log(path.join(root, "dist"));
