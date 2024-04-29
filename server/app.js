e8dfda850ed9d784ae146c99569962fd8f007b5d
leoyongyuan <1359195795@qq.com>
2024-04-29 09:13:08 +0800

    chore: test get env variable

--- server/app.js
+++ server/app.js
@@ -3,3 +3,5 @@ const app = express()
 const port = 3000
 app.use(express.static('./dist')) 
 app.listen(port, () => console.log(`服务器 ${port} 开启成功!`))
+const TAG_VERSION_ENV = process.env.TAG_VERSION_ENV;
+console.log(`My variable is: ${TAG_VERSION_ENV}`);
