2ab31a0f9ee78d7c0eea132d31df6ec1e9ad095e
leoyongyuan <1359195795@qq.com>
2024-04-29 18:01:34 +0800

    chore: update server app.js

--- server/app.js
+++ server/app.js
@@ -2,10 +2,11 @@ const express = require("express");
 const path = require("path");
 const app = express();
 const port = 3000;
-app.use(express.static(path.join(__dirname, "dist")));
+app.use(express.static("./dist"));
 app.listen(port, () => console.log(`服务器 ${port} 开启成功!`));
 app.get("*", (req, res) => {
-	res.sendFile(path.join(__dirname, "dist", "index.html"));
+	res.sendFile(path.join("./", "dist", "index.html"));
 });
 const TAG_VERSION_ENV = process.env.TAG_VERSION_ENV;
 console.log(`My variable is: ${TAG_VERSION_ENV}`);
+console.log(path.join(__dirname, "dist"), __dirname);
